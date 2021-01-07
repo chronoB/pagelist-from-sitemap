79e972865b2518c58c9e678a96219cd21f2a070f
chronoB-Laptop <finn.o.bayer@gmail.com>
2021-01-07 18:17:14 +0100

    get sitemap

--- getpagelist.sh
+++ getpagelist.sh
@@ -24,40 +24,90 @@ then
     exit 1
 fi
 
+# ---------------------------------------------------------------
+
+cleanWebsite(){
+    strippedString=$1
+    if [[ $1 =~ ^(http:) ]] || [[ $1 =~ ^(https:) ]];
+    then
+        strippedString=$(echo $1| cut -d'/' -f 3)
+    fi
+    CLEAN=${strippedString//[^a-zA-Z0-9_.-]/}
+    echo $CLEAN
+}
+
+getURLs (){
+    while read_dom; do
+        if [[ $ENTITY = "url" ]]; then
+            echo $CONTENT
+            exit
+        fi
+    done < $1 > $2
+}
+
+#function to parse the xml
+# see https://stackoverflow.com/questions/893585/how-to-parse-xml-in-bash
+read_dom () {
+    local IFS=\>
+    read -d \< ENTITY CONTENT
+}
+
+create_dir(){
+    n=$((n+1))
+    mkdir -p "$outputdir/urls/"
+    direc="$outputdir/urls/$1"
+    mkdir -p "$direc"
+    echo $direc
+}
+
+curlSitemap(){
+    curl -L "$1/sitemap.xml" -o "$2/sitemap.xml"
+    echo $?
+}
+
+forceHttps(){
+    echo "https://$1"
+}
+
+getSitemap (){
+    cleanWebsite=$(cleanWebsite "$1")
+    printf "Crawling: $cleanWebsite\n"
+
+    httpsWebsitename=$(forceHttps "$cleanWebsite")
+
+    printf "Crawling: $httpsWebsitename\n"
+    #creating directory
+    directory=$(create_dir $cleanWebsite)
+
+    err=$(curlSitemap "$httpsWebsitename" "$directory")
+    if [ $err -eq 1 ];
+    then
+        echo -e "\e[31mError while fetching sitemap. Are you sure there is a sitemap?\e[0m" >&2
+    fi
+}
+
+# ---------------------------------------------------------------
+
 echo ""
 echo "filename: $filename";
 echo "websitename: $websitename";
 echo "Exporting urls to: $outputdir/urls/"
 echo ""
 
-
+#functionality if filename is used
 if [ "$filename" != "not set"  ];
 then
     n=1
     while IFS= read -r line
     do
-        CLEAN=${line//[^a-zA-Z0-9_.-]/}
-        # reading each line
-        echo "Line No. $n : $CLEAN"
-        n=$((n+1))
-        mkdir -p "$outputdir/urls/"
-        direc="$outputdir/urls/"
-        mkdir -p "$direc$CLEAN"
-        wget -nd --spider --force-html -r -l3 $CLEAN 2>&1 | egrep -o 'https?://[^ ]+' | grep -v '\.\(css\|js\|png\|gif\|jpg\|svg\|xml\|otf\|ttf\|mp3\)$' | grep -v '\(oembed\|\?ver=\|\?v=\|eot\)' | grep '\/$' | sort | uniq > $outputdir/urls/$CLEAN/urls.txt
+        getSitemap $line
     done < $filename
 fi
 
+#Functionality if websitename is set
 if [ "$websitename" != "not set"  ];
 then
-
-    CLEAN=${websitename//[^a-zA-Z0-9_.-]/}
-    # reading each line
-    echo "Crawling: $CLEAN"
-    n=$((n+1))
-    mkdir -p "$outputdir/urls/"
-    direc="$outputdir/urls/"
-    mkdir -p "$direc$CLEAN"
-    wget -nd --spider --force-html -r -l3 $CLEAN 2>&1 | egrep -o 'https?://[^ ]+' | grep -v '\.\(css\|js\|png\|gif\|jpg\|svg\|xml\|otf\|ttf\|mp3\)$' | grep -v '\(oembed\|\?ver=\|\?v=\|eot\)' | grep '\/$' | sort | uniq > $outputdir/urls/$CLEAN/urls.txt
-
+    getSitemap $websitename
 fi
+
 exit 0
